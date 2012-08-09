a6d914888c7a95821e2f20ad6ce96f16d9d91515
weed <weed_7777@yahoo.co.jp>
2012-08-09 11:03:02 +0900

    Initial Commit

---
+++ TableCellToView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TableCellToView
+//
+//  Created by  on 12/08/09.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
